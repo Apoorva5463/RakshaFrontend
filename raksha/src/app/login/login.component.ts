2171d7d7db4c2f458723ad554d5134e79e6caa55
GAGAN Jangid <gaganjangid789@gmail.com>
2021-06-15 13:09:26 +0530

    login component

--- raksha/src/app/login/login.component.ts
+++ raksha/src/app/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserLogin } from 'src/UserLogin.model';
+import { UserloginService } from '../userlogin.service';
+
 
 @Component({
   selector: 'app-login',
@@ -6,10 +10,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  userLogin: UserLogin = new UserLogin();
+  public otpdata: any;
+  public email : string = '';
+  public mobile : string = '';
+  public enteredOtp : number = 0;
+  public otpstatus: any ={
+    otp: '',
+    status:''
+  };
 
-  constructor() { }
+  constructor(private userLoginService: UserloginService, private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  clickOnGetOTP(){
+    this.userLoginService.getOtp(this.userLogin).then((data: any) =>{
+      this.otpdata = data;
+      this.otpstatus.otp=this.otpdata.otp;
+      this.otpstatus.status=this.otpdata.status;
+      if(this.otpstatus.status === "OTP IS NOT SENT"){alert('Email id is incorrect');}
+    });
+
+  }
+
+  otpVerification(){
+    if(this.otpstatus.status === "OTP IS SENT"){
+      if(this.enteredOtp == this.otpstatus.otp){
+        this.router.navigate(['/insurance'])
+        alert('Login Successful');
+        console.log("Success");
+        this.router.navigate(['/admin'],{ queryParams: {email: this.userLogin.emailId, mobile: this.userLogin.mobileNo}})
+      }
+      else{
+        alert("Wrong otp");
+      }
+    }
   }
 
 }
